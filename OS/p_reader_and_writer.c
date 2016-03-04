61703265fe94afb3d6ea285b435fb8c467bb9a68
light94 <mishra.rahul1712@gmail.com>
2016-03-04 21:50:29 +0530

    Add mutex and condition variable

--- OS/p_reader_and_writer.c
+++ OS/p_reader_and_writer.c
@@ -1,24 +1,33 @@
-#include <stdio.h>
 #include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #define NUM_READERS 5
 #define NUM_WRITERS 5
+#define NUM_READS 2
+#define NUM_WRITES 2
 int add=0;
 
 int gReaders = 0;
-pthread_mutex_t *mutex;
+int gWaitingReaders = 0; // to give privilege to readers
+pthread_mutex_t mutex= PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t rCond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t wCond = PTHREAD_COND_INITIALIZER;
 
 
 void *readerFunc (void *args);
 void *writerFunc (void *args);
 
-int main(){
+int main(int argc,char *argv){
 	int i;
 	int readerData[NUM_READERS];
 	int writerData[NUM_WRITERS];
 
 	//initialising the mutex
-	//pthread_mutex_init()
+	//pthread_mutex_init(&mutex, NULL);
+	//pthread_cond_init(&rCond, NULL);
+	//pthread_cond_init(&wCond, NULL);
 	// creating the threads
 	pthread_t readerThreads[NUM_READERS];
 	pthread_t writerThreads[NUM_WRITERS];
@@ -48,16 +57,59 @@ int main(){
 }
 
 void *readerFunc (void *args){
-
-	++gReaders;
+	int i = 0;
 	int *data = args;
-	printf("(R) Data in thread: %d \n", *data);
-	printf("Number of readers present: %d \n", gReaders);
+	for (i = 0; i < NUM_READS; i++) {
+	
+		pthread_mutex_lock(&mutex);
+		++gWaitingReaders;
+		while (gReaders == -1) {
+			pthread_cond_wait (&rCond,&mutex);
+		}
+		++gReaders;
+		--gWaitingReaders;
+		pthread_mutex_unlock(&mutex);
+		
+		printf("(R) Data in thread: %d \n", *data);
+		printf("Number of readers present: %d \n", gReaders);
+
+		pthread_mutex_lock(&mutex);
+		--gReaders;
+		// priority implementation
+		if (gWaitingReaders == 0) {
+			pthread_cond_signal(&wCond);	
+		}
+		pthread_mutex_unlock(&mutex);	
+	}
+
+	pthread_exit(0);
 }
 
-void *writerFunc (void *args){
+void *writerFunc (void *args) {
+	int i = 0;
 	int *data = args;
-	printf("(W) Data in thread: %d \n", *data);
-	printf("Number of readers present: %d \n", gReaders);
+	for (i = 0; i < NUM_WRITES; i++) {
+
+		pthread_mutex_lock(&mutex);
+		while (gReaders != 0) {
+			pthread_cond_wait (&wCond,&mutex);
+		}
+		gReaders = -1;
+		pthread_mutex_unlock(&mutex);
+
+		printf("(W) Data in thread: %d \n", *data);
+		printf("Number of readers present: %d \n", gReaders);
+
+		pthread_mutex_lock(&mutex);
+		gReaders = 0;
+		// priority implementation
+		if (gWaitingReaders)
+			pthread_cond_broadcast(&rCond);
+		else
+			pthread_cond_signal(&wCond);	
+		pthread_mutex_unlock(&mutex);
+	}
+
+	pthread_exit(0);
 }
 

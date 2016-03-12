ea8a6e9cc13d9ab114de12d236e1c92fa2db5ae4
light94 <mishra.rahul1712@gmail.com>
2016-03-12 08:19:04 +0530

    Handle Ctrl-C and Ctrl-V

--- OS/shell.c
+++ OS/shell.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 
+void handle_signal(int signo){
+	printf("\n[SHELL ]");
+	fflush(stdout);
+}	
 
 int main(int argc, char* argv[], char* envp[]){
+	signal(SIGINT, handle_signal);
 	char c = '\0';
 	printf("\n[SHELL ]");
 	while(c!=EOF){
